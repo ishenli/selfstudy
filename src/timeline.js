01dfc566b890d38c24ec88aa83acaf2d94ff2b6b
ishenli <mesheni@gmail.com>
2012-11-30 22:21:24 +0800

    添加时间轴demo

---
+++ src/timeline.js
@@ -0,0 +1,16 @@
+/**
+ * User: ishenli
+ * Date: 12-11-26
+ * Time: 下午9:55
+ * To change this template use File | Settings | File Templates.
+ */
+define(function(require, exports, module) {
+
+    // add your code
+
+    module.exports = function() {
+        alert('hello example!');
+    };
+
+});
+
