c1fd3073ee201ee4b38b14d1247c8a52516b5060
ishenli <meshenli@gmail.com>
2012-12-16 15:10:55 +0000

    上传下拉组件

---
+++ dropdown/tests/dropdown-spec.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+    var dropdown = require('../src/dropdown');
+
+    describe('dropdown', function() {
+
+        test('normal usage', function() {
+
+        });
+    });
+
+});
+
