59a37741ab7463ea64bcb7a35a2f9a996e9618cf
ishenli <meshenli@gmail.com>
2013-02-04 20:04:45 +0800

    上传scroll 模块

---
+++ scroll/tests/scroll-spec.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+    var scroll = require('../src/scroll');
+
+    describe('scroll', function() {
+
+        test('normal usage', function() {
+
+        });
+    });
+
+});
+
