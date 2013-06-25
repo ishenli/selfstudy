db98b73ae3026d6242460f879c0c3b41d4a8705a
ishenli <meshenli@gmail.com>
2013-06-25 23:20:05 +0800

    完善xhrManager

--- bookDemo/designPattern/lib.js
+++ bookDemo/designPattern/lib.js
@@ -6,7 +6,11 @@
  * To change this template use File | Settings | File Templates.
  */
 
-
+/**
+ * 获取元素
+ * @param id
+ * @returns {HTMLElement}
+ */
 var $=function(id){
     return document.getElementById(id);
 }
@@ -32,15 +36,28 @@ var Interface=function(name,methods){
         this.methods.push(methods[i]);
     }
 }
-
-
+/**
+ * 类式继承
+ * @param subClass
+ * @param superClass
+ */
+var extend=function(subClass,superClass){
+    var F=function(){};
+    F.prototype=superClass.prototype;
+    subClass.prototype=new F();
+    subClass.prototype.constructor=subClass;
+    subClass.superclass=superClass.superclass;
+    if(subClass.prototype.constructor==Object.prototype.constructor){
+        subClass.prototype.constructor=superClass;
+    }
+}
 var SimpleHandler=function(){};
 SimpleHandler.prototype={
     request:function(method,url,callback,postVars){
         var xhr=this.createXhrObject();
         xhr.onreadystatechange=function(){
             if(xhr.readyState!==4) return;
-            (xhr.stauts=200)?callback.success(xhr.responseText,xhr.responseXML):
+            (xhr.stauts===200)?callback.success(xhr.responseText,xhr.responseXML):
                 (callback.failure(xhr.status));
         }
         xhr.open(method,url,true);
@@ -69,9 +86,91 @@ SimpleHandler.prototype={
     }
 }
 
-
+/**
+ * Handler in Queue
+ * @constructor
+ */
+var QueueHandler=function(){
+    this.queue=[];
+    this.requestInProgress=false;
+    this.retryDelay=5;//in seconds
+};
 var Offline=function(){
     this.storedRequests=[];
+}
+extend(QueueHandler,SimpleHandler);
+/**
+ * 在发起新的请求之前先确保所有的请求都已成功处理
+ * @param method
+ * @param url
+ * @param callback
+ * @param postVars
+ * @param override
+ */
+QueueHandler.prototype.request=function(method,url,callback,postVars,override){
+   if(this.requestInProgress&&override){
+       this.queue.push({
+           method:method,
+           url:url,
+           callback:callback,
+           postVars:postVars
+       });
+   }
+   else {
+       var xhr=this.createXhrObject(),that=this;
+       xhr.onreadystatechange=function(){
+           if(xhr.readyState!==4) return;
+           if(xhr.stauts===200){
+               callback.success(xhr.responseText,xhr.responseXML);
+               that.advanceQueue();
+           }else{
+               callback.failure(xhr.stauts);
+               setTimeout(function(){
+                   that.request(method,url,callback,postVars,true)
+               },this.retryDelay*1000);
+           }
+       }
+       xhr.open(method,url,true);
+       if(method!="POST") postVars=null;
+       xhr.send(postVars);
+   }
+};
+
+QueueHandler.prototype.advanceQueue=function(){
+    if(this.queue.length===0){
+        this.requestInProgress=false;
+        return;
+    }
+    var req=this.queue.shift();
+    this.request(req.method,req.url,req.callback,req.postVars,true);
+};
+/**
+ * OfflineHandler Class
+ * @constructor
+ */
+var OfflineHandler=function(){
+    this.storedRequests=[];
+};
+extend(OfflineHandler,SimpleHandler);
+OfflineHandler.prototype.request=function(method,url,callback,postVars){
+    if(xhrManager.isOffline()){ //store the requests until we are online
+        this.storedRequests.push({
+            method:method,
+            url:url,
+            callback:callback,
+            postVars:postVars
+        });
+    }else{
+        this.flushStoredRequest();
+        OfflineHandler.superclass.request(method,url,callback,postVars);
+    }
+};
+
+OfflineHandler.prototype.flushStoredRequest=function(){
+    for(var i,len=this.storedRequests.length;i<len;i++){
+        var req=this.storedRequests[i];
+        OfflineHandler.superclass.request(req.method,req.url,req.callback,req.postVars);
+    }
 }
 /**
  * xhrManager
